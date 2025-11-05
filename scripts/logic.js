1025876bd56cd95a313f32cea2d1e99eecf39cdc
Blargh14 <59298248+Blargh14@users.noreply.github.com>
2025-11-05 13:55:36 -0500

    Fix getHumanChoice() function and start playGame function

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,3 +1,5 @@
+// choice function logic
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
     
@@ -15,10 +17,18 @@ function getHumanChoice() {
     let choice = prompt("Please enter ROCK, PAPER, or SCISSORS.", "rock");
     choice = choice.toLowerCase();
 
-    if (choice != "rock" || choice != "paper" || choice != "scissors") {
+    if (choice != "rock" && choice != "paper" && choice != "scissors") {
         return "rock"; // default to rock even though they say not to handle it.
     } else {
         return choice;
     }
+}
+
+// Main game function
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
 
 }
