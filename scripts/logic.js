dba4b431c2fc57876590fcff3942a8047ad8cda5
Blargh14 <59298248+Blargh14@users.noreply.github.com>
2025-11-05 13:48:39 -0500

    Add human and computer choice funtions from pseudocode

---
+++ scripts/logic.js
@@ -0,0 +1,24 @@
+function getComputerChoice() {
+    let choice = Math.floor(Math.random() * 3);
+    
+    switch (choice) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissors";
+    }
+}
+
+function getHumanChoice() {
+    let choice = prompt("Please enter ROCK, PAPER, or SCISSORS.", "rock");
+    choice = choice.toLowerCase();
+
+    if (choice != "rock" || choice != "paper" || choice != "scissors") {
+        return "rock"; // default to rock even though they say not to handle it.
+    } else {
+        return choice;
+    }
+
+}
